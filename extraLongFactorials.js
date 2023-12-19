be94b727b12ac0547f7d6ee3150009ddd419aadc
Shk123s <shaqeebsk1234@gmail.com>
2023-12-19 15:51:51 +0530

    six

--- extraLongFactorials.js
+++
@@ -1,9 +0,0 @@
-function extraLongFactorials(n) {
-   let answer=BigInt(1) ;
-for (let i = BigInt(1); i <=BigInt(n); i++) {
-    answer *=  i;
-    
-}
- console.log(BigInt(answer).toString())
-}
-extraLongFactorials(25)
